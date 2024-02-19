6cee640002cdbba042d320e86c7e3da9ac0c9fe0
hamzaomaro <Hamza.Omar@microsoft.com>
2024-02-19 21:18:33 +0300

    added polar and cartesian conversion

--- ComplexAlgebra.py
+++ ComplexAlgebra.py
@@ -1,4 +1,4 @@
-import ComplexNum
+from ComplexNum import ComplexNum
 import math
 
 c3 = ComplexNum
@@ -7,27 +7,41 @@ def add(c1, c2):
     c3.a = c1.a + c2.a
     c3.b = c1.b + c2.b
     print(f"({c1.a}, {c1.b}) + ({c2.a}, {c2.b}) = ({c3.a}, {c3.b})")
+    return c3
 
 def sub(c1, c2):
     c3.a = c1.a - c2.a
     c3.b = c1.b - c2.b
     print(f"({c1.a}, {c1.b}) - ({c2.a}, {c2.b}) = ({c3.a}, {c3.b})")
+    return c3
 
 def mul(c1, c2):
     c3.a = c1.a * c2.a - c1.b * c2.b
     c3.b = c1.a * c2.b + c2.a * c1.b
     print(f"({c1.a}, {c1.b}) x ({c2.a}, {c2.b}) = ({c3.a}, {c3.b})")
+    return c3
 
 def div(c1, c2):
     c3.a = (c1.a * c2.a + c1.b * c2.b) / (c2.a**2 + c2.b**2)
     c3.b = (c2.a * c1.b - c1.a * c2.b) / (c2.a**2 + c2.b**2)
     print(f"({c1.a}, {c1.b}) / ({c2.a}, {c2.b}) = ({c3.a}, {c3.b})")
+    return c3
 
-def modulous(c):
+def modulus(c):
     modc = math.sqrt(c.a**2 + c.b**2)
     print(f"The modulous for ({c.a}, {c.b} is: {modc})")
+    return modc
 
 #Geometric name: real-axis reflection
 def conjugate(c):
-    c.b = -1 * c.b
-    print(f"The Complex Conjugate for c1 is: {c.a}, {c.b}")
+    conc = ComplexNum(c.a,(-1 * c.b))
+    print(f"The Complex Conjugate for c1 is: {conc.a}, {conc.b}")
+    return c
+
+def ToPolar(c):
+    polarc = ComplexNum(modulus(c),math.degrees(math.atan(c.a/c.b)))
+    print(f"polar representation of c1 is rho = {polarc.a} and theta = {polarc.b}")
+
+def ToCartesian(c):
+    carc = ComplexNum((c.a * math.cos(c.b)),(c.a * math.sin(c.b)))
+    print(f"cartesian representation of c1 is a = {carc.a} and b = {carc.b}")
--- main.py
+++ main.py
@@ -1,25 +1,27 @@
 import ComplexAlgebra
-import ComplexNum
+from ComplexNum import ComplexNum
 
 print("Let's fucking start")
 
 print("c1.a: ")
-a = int(input())
+a = float(input())
 print("\nc1.b: ")
-b = int(input())
+b = float(input())
 
-c1 = ComplexNum.ComplexNum(a, b)
+c1 = ComplexNum(a, b)
 
 print("\nc2.a: ")
-a = int(input())
+a = float(input())
 print("\nc2.b: ")
-b = int(input())
+b = float(input())
 
-c2 = ComplexNum.ComplexNum(a, b)
+c2 = ComplexNum(a, b)
 
 ComplexAlgebra.add(c1, c2)
 ComplexAlgebra.mul(c1, c2)
 ComplexAlgebra.sub(c1, c2)
 ComplexAlgebra.div(c1, c2)
-ComplexAlgebra.modulous(c1)
+ComplexAlgebra.modulus(c1)
 ComplexAlgebra.conjugate(c1)
+ComplexAlgebra.ToPolar(c1)
+ComplexAlgebra.ToCartesian(c1)
